6169265b1f2bf4a469bc85acea7a7afc51ce61eb
zhouchaoxing <zhouchaoxing@cmiot.chinamobile.com>
2019-10-10 18:07:59 +0800

    引入qmui

--- app/src/main/java/cn/krisez/account/MainActivity.kt
+++ app/src/main/java/cn/krisez/account/MainActivity.kt
@@ -1,18 +1,41 @@
 package cn.krisez.account
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
+import android.view.Gravity
+import android.view.View
+import android.widget.Toast
+import cn.krisez.framework.base.BaseActivity
+import cn.krisez.framework.base.IBaseView
+import cn.krisez.framework.base.Presenter
+import com.bumptech.glide.Glide
+import com.qmuiteam.qmui.widget.QMUIRadiusImageView
 
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(){
+    override fun init(bundle: Bundle?) {
+        setRefreshEnable(true)
+        setUpTopBar()
+        initHead()
+        tv.setOnClickListener {
+            Toast.makeText(this,":asdasd",Toast.LENGTH_SHORT).show()
+        }
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+    private fun initHead() {
+        val head = QMUIRadiusImageView(this)
+        head.isCircle = true
+        mTopBar.addLeftView(head,R.id.main_top_head,mTopBar.generateTopBarImageButtonLayoutParams())
+        mTopBar.addRightImageButton(R.drawable.add,R.id.main_add_group)
+
+        Glide.with(this).load("http://e.hiphotos.baidu.com/image/pic/item/4610b912c8fcc3cef70d70409845d688d53f20f7.jpg").into(head)
+    }
+
+    override fun newView(): View = View.inflate(this,R.layout.activity_main,null)
+
+    override fun presenter(): Presenter? = null
+
+
+    override fun onRefresh() {
     }
 }
